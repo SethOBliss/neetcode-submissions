497903d25cd18d1811d940300d86f7376e7d4b9e
Ayush Seth <sethayush1466@gmail.com>
2026-06-22 14:16:20 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        k=""
+        for i in strs:
+            k= k+"9nwfunf3949f38f9n9"+i
+        print (k)
+        return k
+
+
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        for i in s.split("9nwfunf3949f38f9n9"):
+            res.append(i)
+        return res[1:]
+
